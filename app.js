6d28e7c5293c50c7aa6f51f5163e0d9980bff8e2
Daniel Ng <dng@ruelala.com>
2016-03-24 19:29:43 -0400

    Tweaking the bot to behave like a normal bot

--- app.js
+++ app.js
@@ -8,24 +8,12 @@
 var SLACK_TOKEN = require('./config').slackToken;
 
 var https = require('https');
-var  _ws = require('ws');
-var r = require('./responses');
+var websocket = require('ws');
+var responses = require('./responses');
 
 var counter = 1;
-var ws, slackID;
-
-https.get("https://slack.com/api/channels.create?token=" 
-+ SLACK_TOKEN + "&name=trump", function(res) {
-    var data = "";
-    res.on('data', function(chunk) {
-        data += chunk;
-    }).on('error', function(err) {
-        console.log("Failed to create #trump channel. Verify "
-            + "that you added your API key.");
-    }).on('end', function() {
-        console.log(data);
-    });
-});
+var ignoredChannels = [];
+var ws, slackID, rtm;
 
 https.get("https://slack.com/api/rtm.start?token=" + SLACK_TOKEN, function(res) {
     console.log("Connecting to Slack API...");
@@ -33,54 +21,70 @@ https.get("https://slack.com/api/rtm.start?token=" + SLACK_TOKEN, function(res) 
     res.on('data', function(chunk) {
         data += chunk;
     }).on('error', function(err) {
-    console.log("Failed to connect to Slack. "
-        + "Did you put in your Slack bot's token in app.js?");
+        console.log("Failed to connect to Slack. Did you put in your Slack bot's token in app.js?");
+        console.log(err);
     }).on('end', function() {
-        var rtm = JSON.parse(data);
-        ws = new _ws(rtm.url);
-        slackId = rtm.self.id;
+        rtm = JSON.parse(data);
+        // console.log(rtm);
+        ws = new websocket(rtm.url);
+        slackID = rtm.self.id;
         console.log("Logging into " + rtm.team.name + "'s Slack...");
-        ws.on('open', function() {
-            goTrump(rtm.team.name, rtm.team.prefs.default_channels[0]);
-        });
+        ws.on('open', goTrump);
     })
 });
 
+function goTrump() {
+    console.log("Listening for new messages...");
+    ws.on('message', handleEvent);
+};
 
+function handleEvent(data) {
+    var event = JSON.parse(data);
+    // console.log(event);
+    var index = ignoredChannels.indexOf(event.channel);
+    var response;
+    if (event.type === "message" && event.user !== slackID) {
+        if (index != -1) { // check if the channel is ignored
+            if (event.text === "no wait, come back!") {
+                ignoredChannels.splice(index, 1);
+                console.log("Ignored channels: " + ignoredChannels);
+                respond(event.channel, "LET'S MAKE " + rtm.team.name.toUpperCase() + " GREAT AGAIN!!1!");
+            }
+        } else if (event.text === "please go away") {
+            ignoredChannels.push(event.channel);
+            console.log("Ignored channels: " + ignoredChannels);
+            respond(event.channel, "Sorry, there is no STAR on the stage tonight.");
+        } else if (event.text !== undefined) { // channel isn't ignored and message isn't telling Trump to stfu
+            console.log(event.text);
+            var response = getResponse(event.text);
+            if (response !== undefined) {
+                respond(event.channel, response);
+            }
+        }
+    }
+};
 
-function goTrump(teamName, channelID) {
-    console.log("Donald Trump has joined " + teamName + "!");
+function respond(channelID, message) {
+    console.log("Responding with: " + message);
     ws.send(JSON.stringify({
         "id": counter,
         "type": "message",
         "channel": channelID,
-        "text": "LET'S MAKE " + teamName.toUpperCase() + " GREAT AGAIN."
+        "text": message
     }));
     counter++;
-
-    console.log("Listening for new messages...");
-    ws.on('message', function(data) {
-        var event = JSON.parse(data);
-        if(event.type === "message" && event.user !== slackID) {
-            ws.send(JSON.stringify({
-                "id": counter,
-                "type": "message",
-                "channel": channelID,
-                "text": getResponse(event.text)
-            }))
-        }
-        counter++;
-    });
-}
+};
 
 function getResponse(message) {
-    for(var i = 0; i < r.length; i++) {
-        for(var j = 0; j < r[i].keywords.length; j++) {
-            if(message.toLowerCase().indexOf(r[i].keywords[j]) != -1) {
-                console.log("Responding to message: " + message);
-                return r[i].messages[Math.floor(Math.random() * r[i].messages.length)];
+    message = message.toLowerCase();
+    console.log("Checking message: " + message);
+    for(var i = 0; i < responses.length; i++) {
+        var item = responses[i];
+        for(var j = 0; j < item.keywords.length; j++) {
+            var keyword = item.keywords[j];
+            if(message.toLowerCase().indexOf(keyword) != -1) {
+                return item.messages[Math.floor(Math.random() * item.messages.length)];
             }
         }
     }
-}
-
+};
